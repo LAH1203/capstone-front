579009cd0b0f0affbde70e6a3a6dcd167d006573
이아현 <lah1203@naver.com>
2023-01-20 00:14:01 +0900

    feat: 로그인 msw mocking

--- src/mocks/handlers/auth.js
+++ src/mocks/handlers/auth.js
@@ -1,5 +1,18 @@
 import { rest } from 'msw';
 
-const authHandlers = [];
+import { API_PATH } from '@/constants/path';
+
+const authHandlers = [
+  rest.post(API_PATH.LOGIN, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        new: true,
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkluQm9uZyBTb25nIiwiaWF0IjoxNTE2MjM5MDIyfQ.XikBQw8OOU87xoWsYVTJjx6Vpb114WW4FfBoWqqVYHU',
+      }),
+    );
+  }),
+];
 
 export default authHandlers;

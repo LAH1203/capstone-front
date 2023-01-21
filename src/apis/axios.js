b92159381d5d698fc66e0d1bb0142960d99dcc6b
이아현 <lah1203@naver.com>
2023-01-21 17:48:29 +0900

    fix: baseurl 수정

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 import { accessTokenProvider } from '@/utils/token';
 
-const baseURL = 'https://97405a6b-4168-4347-97f8-ca4732694228.mock.pstmn.io';
+const baseURL = 'https://3ad6dc28-ff1e-4eab-9772-5ef99cccdd64.mock.pstmn.io';
 
 const axios = Axios.create({
   baseURL,

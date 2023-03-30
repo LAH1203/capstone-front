fd9123b6e8dc8cebb4b291935fcac6b50def7455
이아현 <lah1203@naver.com>
2023-03-30 17:15:26 +0900

    refactor: 사용하지 않는 파일 삭제

--- src/components/Input/index.styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-const Input = styled.input`
-  border: 1px solid ${({ theme: { colors } }) => colors.GRAY_700};
-  border-radius: 4px;
-`;
-
-export { Input };

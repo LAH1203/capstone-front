7587faafccc75c1819001bcc421b8d87d5b185b8
이아현 <lah1203@naver.com>
2023-05-22 15:38:26 +0900

    fix: 요일이 잘못 나오던 오류

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,3 @@
-const getDay = day => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].at(day);
+const getDay = day => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].at(day);
 
 export { getDay };

ca430842fddc6c460cadec5e6e993969661dd32c
이아현 <lah1203@naver.com>
2023-05-24 15:11:01 +0900

    feat: 사용할 폰트 추가 및 기존에 사용하던 웹 폰트를 로컬 폰트로 변경

--- src/styles/font.js
+++ src/styles/font.js
@@ -1,10 +1,73 @@
 import { css } from '@emotion/react';
 
+const { origin } = window.location;
+
 const fontStyle = css`
   @font-face {
-    font-family: 'IBMPlexSansKR-Regular';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff')
-      format('woff');
+    font-family: 'IBMPlexSansKR';
+    src: url('${origin}/font/IBMPlexSansKR-Regular.woff2') format('woff2'),
+      url('${origin}/font/IBMPlexSansKR-Regular.woff') format('woff'),
+      url('${origin}/font/IBMPlexSansKR-Regular.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'NanumSquareNeo';
+    src: url('${origin}/font/NanumSquareNeo-Variable.woff2') format('woff2'),
+      url('${origin}/font/NanumSquareNeo-Variable.woff') format('woff'),
+      url('${origin}/font/NanumSquareNeo-Variable.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'SeoulNamsan';
+    src: url('${origin}/font/SeoulNamsanM.woff2') format('woff2'),
+      url('${origin}/font/SeoulNamsanM.woff') format('woff'),
+      url('${origin}/font/SeoulNamsanM.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'MaruBuri';
+    src: url('${origin}/font/MaruBuri-Regular.woff2') format('woff2'),
+      url('${origin}/font/MaruBuri-Regular.woff') format('woff'),
+      url('${origin}/font/MaruBuri-Regular.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'IMHyemin';
+    src: url('${origin}/font/IM_Hyemin-Regular.woff2') format('woff2'),
+      url('${origin}/font/IM_Hyemin-Regular.woff') format('woff'),
+      url('${origin}/font/IM_Hyemin-Regular.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Diary';
+    src: url('${origin}/font/EF_Diary.woff2') format('woff2'),
+      url('${origin}/font/EF_Diary.woff') format('woff'),
+      url('${origin}/font/EF_Diary.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'UhBeeZZIBA';
+    src: url('${origin}/font/UhBeeZZIBA-Regular.woff2') format('woff2'),
+      url('${origin}/font/UhBeeZZIBA-Regular.woff') format('woff'),
+      url('${origin}/font/UhBeeZZIBA-Regular.ttf') format('truetype');
     font-weight: normal;
     font-style: normal;
     font-display: swap;

63511b2af7d996036d1e8601019ce0697c6a0234
이아현 <lah1203@naver.com>
2023-07-11 16:04:45 +0900

    feat: 날짜 변환 유틸 함수 생성

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,13 @@
 const getDay = day => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].at(day);
 
-export { getDay };
+const getDayKorean = day => ['일', '월', '화', '수', '목', '금', '토'].at(day);
+
+const convertDate = dateLinkByStick => {
+  const [year, month, date] = dateLinkByStick.split('-');
+
+  return `${year}년 ${month}월 ${date}일 ${getDayKorean(
+    new Date(year, month - 1, date).getDay(),
+  )}요일`;
+};
+
+export { getDay, convertDate };

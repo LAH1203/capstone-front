581e3679434578f09a9064e6851fe2ab069e3ecf
이아현 <lah1203@naver.com>
2023-07-23 02:21:10 +0900

    feat: 검색어 디바운스 추가

--- src/components/SearchModal/index.jsx
+++ src/components/SearchModal/index.jsx
@@ -1,77 +1,39 @@
-import { useEffect, useRef, useState } from 'react';
+import { Suspense, useState } from 'react';
 
 import { AiOutlineSearch } from 'react-icons/ai';
-import { useInfiniteQuery } from 'react-query';
-import { useNavigate } from 'react-router-dom';
 
 import * as S from './index.styles';
+import List from './List';
 
-import { searchDiary } from '@/apis/request/diary';
+import ErrorBoundary from '@/components/ErrorBoundary';
 import Portal from '@/components/Portal';
-import { BROWSER_PATH } from '@/constants/path';
 import useClosing from '@/hooks/useClosing';
+import useDebounce from '@/hooks/useDebounce';
 import useInput from '@/hooks/useInput';
 import useModal from '@/hooks/useModal';
 
 const modalAnimationTime = 150;
 
 const SearchModal = () => {
-  const { value, onChangeValue } = useInput('');
+  const { value, dangerouslySetValue } = useInput('');
   const [pageNumber, setPageNumber] = useState(0);
 
   const { setOffModal } = useModal();
   const { isClosing, close } = useClosing(modalAnimationTime, setOffModal);
 
-  const target = useRef(null);
-
-  const { data, fetchNextPage, isFetching } = useInfiniteQuery({
-    queryKey: ['search', value, pageNumber],
-    queryFn: ({ pageParam = 0 }) => searchDiary(value, pageParam),
-    getNextPageParam: page => {
-      const res = page.isLastPage ? undefined : Number(page.pageNumber) + 1;
-
-      return res;
-    },
-  });
-
-  const navigate = useNavigate();
+  const debounce = useDebounce();
 
   const preventBubbling = e => {
     e.stopPropagation();
   };
 
-  const goToDetailPage = diaryId => () => {
-    close();
-    navigate(`${BROWSER_PATH.DETAIL}/${diaryId}`);
+  const increasePageNumber = () => {
+    setPageNumber(prev => prev++);
   };
 
-  useEffect(() => {
-    const onIntersection = async ([entry], observer) => {
-      if (
-        !entry.isIntersecting ||
-        isFetching ||
-        data.pages[pageNumber].isLastPage
-      )
-        return;
-
-      fetchNextPage().then(() => {
-        if (data.pages[pageNumber].isLastPage) {
-          observer.unobserve(target.current);
-        }
-        setPageNumber(prev => prev++);
-      });
-    };
-    const observer = new IntersectionObserver(onIntersection, {
-      threshold: 0.5,
-    });
-    observer.observe(target.current);
-
-    return () => observer.disconnect();
-  }, [data.pages, fetchNextPage, isFetching, pageNumber]);
-
-  const diaries = data.pages.reduce((accumulator, currentObject) => {
-    return accumulator.concat(currentObject.list);
-  }, []);
+  const debouncedChangeValue = debounce(e => {
+    dangerouslySetValue(e.target.value);
+  }, 1000);
 
   return (
     <Portal to="modal">
@@ -89,39 +51,20 @@ const SearchModal = () => {
             <AiOutlineSearch />
             <input
               type="text"
-              value={value}
-              onChange={onChangeValue}
+              onChange={debouncedChangeValue}
               placeholder="제목 또는 내용 검색"
             />
           </S.InputContainer>
-          <S.List>
-            {diaries.map(diary => (
-              <S.Item
-                onClick={goToDetailPage(diary.diaryId)}
-                key={diary.diaryId}
-              >
-                <S.Title>
-                  <p>{diary.title}</p>
-                  <p>{diary.date}</p>
-                </S.Title>
-                <S.Content>{diary.content}</S.Content>
-                <S.HashtagWrapper>
-                  {diary.hashtag.map(hashtag => (
-                    <S.Hashtag key={hashtag}>{hashtag}</S.Hashtag>
-                  ))}
-                </S.HashtagWrapper>
-              </S.Item>
-            ))}
-            <div ref={target}>
-              {isFetching && (
-                <S.Loading>
-                  <p />
-                  <p />
-                  <p />
-                </S.Loading>
-              )}
-            </div>
-          </S.List>
+          <ErrorBoundary fallback={<div>일기를 불러오는 데 실패했어요.</div>}>
+            <Suspense fallback={<div>일기를 불러오는 중...</div>}>
+              <List
+                text={value}
+                pageNumber={pageNumber}
+                increasePageNumber={increasePageNumber}
+                close={close}
+              />
+            </Suspense>
+          </ErrorBoundary>
         </S.Container>
       </S.Dimmer>
     </Portal>

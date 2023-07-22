0f70047298b2a3227341e0e215de84c6aa029707
Jaeseok <72875894+Pridesd@users.noreply.github.com>
2023-07-23 00:30:34 +0900

    메인 페이지 내 미니 캘린더 구현 (#36)
    * feat: 미니 캘린더 구현

--- src/pages/Main/SmallCalendar/index.jsx
+++ src/pages/Main/SmallCalendar/index.jsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from 'react';
+
+import { RxDotFilled } from 'react-icons/rx';
+import { useQuery } from 'react-query';
+
 import * as S from './index.styles';
 
+import { getdiaryLisyByCalendar } from '@/apis/request/diary';
+import { simplifyDiaryForCalendar } from '@/utils/diaries';
+
 const SmallCalendar = () => {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth() + 1;
+  const totalDate = new Date(year, month, 0).getDate();
+  const startDate = new Date(year, month - 1, 1).getDay() + 1;
+
+  const [currentData, setCurrentData] = useState({});
+
+  const { data } = useQuery({
+    queryKey: ['diaryList'],
+    queryFn: () => getdiaryLisyByCalendar(year, month),
+  });
+
+  useEffect(() => {
+    const simplifiedDiaries = simplifyDiaryForCalendar(data, year, month);
+    setCurrentData(simplifiedDiaries);
+  }, [data]);
+
   return (
-    <S.Container>
-      <h1>SmallCalendar</h1>
+    <S.Container to="/calendar">
+      <S.Title>
+        <span>{year}년</span>
+        <span>{month}월</span>
+      </S.Title>
+      <S.Calendar>
+        {Array.from(Array(totalDate).keys()).map(i => (
+          <S.Date key={`${i}-day`} date={i} startdate={startDate}>
+            <S.DateInfo date={i} startDate={startDate}>
+              {i + 1}
+            </S.DateInfo>
+            <span>{currentData[i + 1] && <RxDotFilled color="#404F40" />}</span>
+          </S.Date>
+        ))}
+      </S.Calendar>
     </S.Container>
   );
 };
--- src/pages/Main/SmallCalendar/index.styles.jsx
+++ src/pages/Main/SmallCalendar/index.styles.jsx
@@ -1,9 +1,51 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
-const Container = styled.div`
+const Container = styled(Link)`
+  box-sizing: border-box;
+  padding: 1rem;
   width: 100%;
   height: 15rem;
-  background-color: #81adff;
+  background-color: ${({ theme }) => theme.colors.INPUT_BACKGROUND};
+  font-size: 0.825rem;
 `;
 
-export { Container };
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.25rem;
+  font-weight: 600;
+`;
+
+const Calendar = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  margin-top: 1rem;
+`;
+
+const Date = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.25rem;
+  min-height: 2rem;
+  grid-column-start: ${({ date, startdate }) =>
+    date === 0 ? startdate : 'auto'};
+  font-weight: 600;
+`;
+
+const DateInfo = styled.span`
+  color: ${({ date, startDate, theme }) => {
+    switch ((date + startDate) % 7) {
+      case 1:
+        return theme.colors.RED_500;
+      case 0:
+        return theme.colors.BLUE_500;
+      default:
+        return theme.colors.GREEN_700;
+    }
+  }};
+`;
+
+export { Container, Title, Calendar, Date, DateInfo };
